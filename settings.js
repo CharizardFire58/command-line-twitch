d80d89d987ccd920b222676e7d8570bd9712e7c0
Mark Lehane <mklehane@gmail.com>
2016-04-13 10:25:04 +0100

    stuff

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
-module.exports.channel = "maddog738";
-module.exports.bot_name = "ben_matbot";
-module.exports.bot_oauth = "oauth:egxnzfj2so25sur3xeklmbmyado4et";
+module.exports.channel = "<YOUR CHANNEL NAME>";
+module.exports.bot_name = "YOUR BOT NAME";
+module.exports.bot_oauth = "YOUR BOT OAUTH";
