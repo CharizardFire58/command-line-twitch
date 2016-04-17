07d8479f3a643c189acaeeb9f784bd98cb161940
Mark Lehane <mklehane@gmail.com>
2016-04-17 22:02:50 +0100

    I'm sick of writing commit messages :/

--- settings.js
+++ settings.js
@@ -1,3 +1,8 @@
-module.exports.channel = "<YOUR CHANNEL NAME>";
-module.exports.bot_name = "YOUR BOT NAME";
-module.exports.bot_oauth = "YOUR BOT OAUTH";
+// module.exports.channel = "<YOUR CHANNEL NAME>";
+// module.exports.bot_name = "YOUR BOT NAME";
+// module.exports.bot_oauth = "YOUR BOT OAUTH";
+
+module.exports.user = "maddog738";
+module.exports.channel = "maddog738";
+module.exports.bot_name = "ben_matbot";
+module.exports.bot_oauth = "oauth:egxnzfj2so25sur3xeklmbmyado4et";
